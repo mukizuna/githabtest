6d9761e94f64e0fe9a712e8e3e3c8d1d0fd858b7
DESKTOP-TQ7OKHA\mukizuna <mukizuna35@gmail.com>
2017-09-07 11:35:27 +0900

    Ｑキーを押すとデバックログに”Ｑ”が表示される

--- Assets/test.cs
+++ Assets/test.cs
@@ -11,6 +11,9 @@ public class test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q");
+        }
 	}
 }
